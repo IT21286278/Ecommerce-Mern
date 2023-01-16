d29054434783b6b32bfe19a2cf25dc6373b9d07c
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2023-01-16 23:36:05 +0530

    Lesson-5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data';
+
 function App() {
   return (
     <div>
@@ -5,7 +7,30 @@ function App() {
         <a href="/">Amazona</a>
       </header>
 
-      <main>List products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {
+            //map funtion to convert product to a jsx file
+            data.products.map((product) => (
+              <div key={product.slug} className="product">
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name}></img>
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>{product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </main>
     </div>
   );
 }
