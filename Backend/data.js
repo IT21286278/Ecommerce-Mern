7a267130646a04779023e5741564e035422c7433
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2023-01-23 14:36:14 +0530

    Lesson-7-Node-Server

---
+++ Backend/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Nike slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', //67px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality shirt',
+    },
+
+    {
+      name: 'Adidas fit shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 240,
+      countInStock: 20,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 16,
+      description: 'High quality shirt',
+    },
+
+    {
+      name: 'Nike slim pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 250,
+      countInStock: 12,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      description: 'High quality Pant',
+    },
+
+    {
+      name: 'Puma slim pant',
+      slug: 'puma-slim-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 180,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.4,
+      numReviews: 22,
+      description: 'High quality Pants',
+    },
+  ],
+};
+
+export default data;
